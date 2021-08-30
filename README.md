606e5eeab27948c2452ffd1c760e7e6a9cab4a75
Nathan Kwabena Fletcher <fletcher.nathan.a@gmail.com>
2021-08-30 14:54:41 +0000

    updated docs and linked new UI. Testing animations

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # kindly
+
+# Development
+Please make sure that your development environment has the following prerequisites:
+- NodeJS 16
+- Python 3
+    - Using a command-line terminal of your choice, `cd` into the api folder.
+    - Run the following commands to complete your environment
+    - `pip install --upgrade pip`
+    - `pip install tensorflow`
+    - `pip install -r requirements.txt`
+    - Install pytorch by going to (their website)[https://pytorch.org/get-started/locally/] to know how to do it with your operating system. However, for this mac configuration in this project you can use `pip` by running `pip install torch torchvision`
+## Running Locally
+From the root of the application, run the python server using the following command `python api/flasksample.py`
+
+In a new terminal tab `cd` into the client folder and and run the following commands
+- `npm i`
+- `npm run dev`
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,49 +1,60 @@
 <template>
-<div id="home">
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card>
-        <v-card-title class=""> </v-card-title>
-        <v-card-text>
-          <p class="headline text-center text-md-center">KINDLY</p>
-          <v-col cols="12">
-            <v-text-field
-              v-model="text"
-              label="What do you want to say?"
-              outlined
-            ></v-text-field>
-          </v-col>
-        </v-card-text>
-        <v-card-actions>
-          <!-- width and height are optional 
+  <div id="home">
+    <v-row justify="center" align="center">
+      <v-col cols="12" sm="8" md="6">
+        <v-card>
+          <v-card-title class=""> </v-card-title>
+          <v-card-text>
+            <p class="headline text-center text-md-center">KINDLY</p>
+            <v-col cols="12">
+              <v-text-field
+                v-model="text"
+                label="What do you want to say?"
+                outlined
+              ></v-text-field>
+            </v-col>
+          </v-card-text>
+          <v-card-actions>
+            <!-- width and height are optional 
           https://medium.com/@fonto.design/using-lottie-in-nuxt-js-554949d19063-->
-          <lottie
-            v-if="loading"
-            :width="100"
-            :height="100"
-            :options="lottieOptions"
-            v-on:animCreated="handleAnimation"
-          />
-          <v-spacer />
-          <v-btn
-            color="primary"
-            :loading="loading"
-            :disabled="loading"
-            @click="obtainSentiment"
-          >
-            check
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
-</div>
-  
+            <lottie
+              v-if="loading"
+              :width="100"
+              :height="100"
+              :options="lottieOptions"
+              v-on:animCreated="handleAnimation"
+            />
+            <div>
+              <lottie
+                v-if="sentimentResult"
+                :width="100"
+                :height="100"
+                :options="lottieOptions"
+                v-on:animCreated="handleAnimation"
+              />
+              <p>{{ sentimentMessage }}</p>
+            </div>
+
+            <v-spacer />
+            <v-btn
+              color="primary"
+              :loading="loading"
+              :disabled="loading"
+              @click="obtainSentiment"
+            >
+              check
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 <script>
 import Lottie from "vue-lottie/src/lottie.vue";
 import * as animationData from "../assets/animation.json";
-
+import * as positiveAnimation from "../assets/happy.json";
+import * as negativeAnimation from "../assets/sad.json";
 export default {
   components: {
     lottie: Lottie,
@@ -55,11 +66,33 @@ export default {
       lottieOptions: { animationData: animationData.default },
       animationSpeed: 1,
       anim: null,
+      sentimentResult: false,
+      sentimentMessage: "",
     };
   },
   methods: {
     async obtainSentiment() {
+      let vm = this;
       this.loading = true;
+      let sentiment = await this.$axios.post("localhost:8080/detect", {
+        text: vm.text,
+      });
+      if (vm.sentimentResult) {
+        let pos = data.result["not-offensive"];
+        let neg = sentiment.result["offensive"];
+        if (
+          parseFloat(sentiment.result["offensive"]) >
+          parseFloat(sentiment.result["offensive"])
+        ) {
+          vm.lottieOptions = { animationData: positiveAnimation.default },
+          vm.sentimentMessage = "That's not a very nice thing to say 😾 ";
+        } else {
+          vm.lottieOptions = { animationData: negativeAnimation.default },
+           vm.sentimentMessage =  "Great, you can post that!";
+        }
+      }
+      // Result sad face : https://assets10.lottiefiles.com/packages/lf20_pojzngga.json
+      // Result happy face: https://assets7.lottiefiles.com/packages/lf20_sgzw5ogf.json
     },
     handleAnimation(anim) {
       this.anim = anim;
