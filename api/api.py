d7f51e7db688bda0dc7a00e936b9f7fcf3534222
sabinevidal <svidal@live.co.za>
2021-10-25 20:09:20 +0200

    Removing model debugging

--- api/api.py
+++ api/api.py
@@ -116,9 +116,9 @@ def process(input_text):
     file_path.close()
 
     # pylint: disable=no-value-for-parameter
-    model = AutoModelForSequenceClassification.from_pretrained("cardiffnlp/twitter-roberta-base-offensive")
+    model = AutoModelForSequenceClassification.from_pretrained('./model')
     # model.save_pretrained(MODEL)
-    tokenizer = AutoTokenizer.from_pretrained("cardiffnlp/twitter-roberta-base-offensive")
+    tokenizer = AutoTokenizer.from_pretrained('./model')
     encoded_input = tokenizer(preprocess(input_text), return_tensors='pt')
     output = model(**encoded_input)
 
