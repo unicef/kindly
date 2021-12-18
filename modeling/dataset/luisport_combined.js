20f2ee497d5ae119de149df2c613fa1694836f8d
sabinevidal <svidal@live.co.za>
2021-12-18 17:07:58 +0200

    Comment update

--- modeling/dataset/luisport_combined.js
+++ modeling/dataset/luisport_combined.js
@@ -1,4 +1,10 @@
-/* This script is what was used to extract data from the kindly_luis_export.json file, combined test and label as '[text], [label_number]'*/
+/*
+This script is what was used to extract data from the kindly_luis_export.json file, combined test and label as 
+`{
+    "text": "[text]",
+    "intent": [0 or 1]
+}`
+*/
 
 const fs = require('fs')
 let louis = require('./kindly_luis_export.json')
